8af579210f33751ea900d8140e1ec90cefa67096
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-10-19 14:21:53 +0530

    reverted

--- Upload/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Upload_files
-class Files_Form(forms.ModelForm):
-    class Meta:
-        model = Upload_files
-        fields = [
-        'files'
-        ]
--- Upload/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-class Upload_files(models.Model):
-    files = models.FileField()
